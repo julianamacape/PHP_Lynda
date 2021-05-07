4b8596c1741bd2bbab65ed70d0da2c7dcf55ac3e
Juliana Pereira <jotace1234@hotmail.com>
2021-05-07 12:04:26 -0300

    updated

--- 01BASICS/Ex_Files_Learning_PHP/Exercise Files/01_07_challenge/start.php
+++ 01BASICS/Ex_Files_Learning_PHP/Exercise Files/01_07_challenge/start.php
@@ -4,3 +4,17 @@ $city = "NYC";
 $movie = "Star Wars"; //Italics
 $friend = "George"; //Bold
 $candy = "Sour Patch Kids";
+
+/* The idea of the challenge is to print on the screen a story of a woman who's going to
+a certain city to watch a certain movie with her friend and
+they're gonna buy their favorite candy.
+Use the variables and follow the guidelines recommended on the side comments.
+*/
+
+echo '<h1>Hi, my name\'s '.$name.'.</h1> <br/> This weekend I\'m going 
+to '. $city . ' to meet my good friend ' . '<b>'.$friend.'</b> and 
+we\'re going to watch <i>'.$movie.'</i> together. On our way to the cinema, we\'ll stop by the
+deli to buy a pack of '.$candy.'.' 
+
+//Done!
+?>
